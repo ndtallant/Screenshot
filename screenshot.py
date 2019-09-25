c59dde0607c91118eb80987d04842d73a76db2be
Nick Tallant <ndtallant@uchicago.edu>
2019-09-25 11:20:51 -0500

    Added adblock and better cleaning

--- screenshot.py
+++ screenshot.py
@@ -1,42 +1,55 @@
+import os
 import re
 import time
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 
 class ScreenShotter:
 
-    def __init__(self, urls):
-        self.urls = self.read_file(urls)
-        self.driver = webdriver.Firefox()
-        #self.driver.fullscreen_window()
+    def __init__(self, urls, start_position=0, max_time=10, ads=False, fullscreen=False):
+        self.urls = self.filter_urls(self.read_file(urls)[start_position:])
+
+        if ads:
+            self.driver = webdriver.Firefox()
+        else:
+            ffprofile = webdriver.FirefoxProfile()
+            ffprofile.add_extension('adblock_plus-3.6.3-an+fx.xpi')
+            ffprofile.set_preference("extensions.adblockplus.currentVersion", "3.6.3")
+            self.driver = webdriver.Firefox(ffprofile)
+
+        self.driver.set_page_load_timeout(max_time)
+        if fullscreen:
+            self.driver.fullscreen_window()
 
     def read_file(self, f):
+        '''Returns a list of urls from a text file or urls.'''
         with open(f, 'r') as f:
-            return f.readlines()
+            return f.read().splitlines()
 
-    def verify_link_from_path(path):
-        '''Takes in a selenium xpath and appends the href to self.extra_urls.'''
-        link = path.get_attribute('href')
-        not_these = ['?', 'jpg', 'png', 'pdf', 'download', 'csv', 'svg']
-        if any(part in link for part in not_these):
-            return
-        return link
+    def filter_urls(self, urls):
+        '''Takes in a list of urls and filters out sketchy content.'''
+        bads = ['?', 'jpg', 'png', 'pdf', 'download', 'csv', 'svg']
+        return list(filter(lambda url: not any(part in url for part in bads), urls))
 
     def run(self):
         with self.driver as driver:
-            for url in self.urls:
+            for index, url in enumerate(self.urls):
                 self.grab_url(driver, url)
 
     def grab_url(self, driver, url):
         try:
-            driver.get('http://'+url)
-            time.sleep(1)
             name = re.sub('\.', '_', url)
-            driver.save_screenshot(f'images/{name}.png')
-            print("Saved", url)
-            #self.extra_urls += [verify_link_from_path(path) for path in driver.find_elements_by_xpath("//a[@href]")]
-        except:
-            print(url, "DID NOT WORK")
+            if name + '.png' not in os.listdir('images'):
+                driver.get('http://'+url)
+                driver.save_screenshot(f'images/{name}.png')
+                print("Saved:", url)
+            else:
+                print("Already collected:", url)
+        except TimeoutException:
+            print("Timed out:", url)
+        except Exception as e:
+            print("Unkown error:", e, url)
 
 if __name__ == "__main__":
-    shotter = ScreenShotter('big_websites.txt')
+    shotter = ScreenShotter('little_websites.txt')
     shotter.run()
